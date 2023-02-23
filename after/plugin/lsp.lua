d1768ba210d67c4772cba55ac938c3cc1e394edc
Avicenna <87450618+AvicennaJr@users.noreply.github.com>
2023-02-23 17:26:19 +0300

    Create lsp.lua

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,27 @@
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = false,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = {buffer = bufnr, remap = false}
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+lsp.setup()
